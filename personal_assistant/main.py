c96eb823ce6c6160342abcf7173ed87976262188
Psychxox <fakifiki878@gmail.com>
2026-08-01 00:56:01 +1000

    Проект 5 глава

--- personal_assistant/main.py
+++ personal_assistant/main.py
@@ -67,12 +67,79 @@ def calculate():
         except ValueError:
             print("Для чисел нужно ввести только цифры!")
 
+tasks = []
+
+def do_add():
+
+    do = input("Введите задачу: ")
+
+    tasks[len(tasks):] = [do]
+    print("Задача добавлена!")
+
+def do_del():
+    global tasks
+    
+    if len(tasks) == 0:
+        print("Список задач пуст!")
+        return
+    
+    do_list()
+    
+    try:
+        num = int(input("Введите номер задачи для удаления: "))
+        
+        if num < 1 or num > len(tasks):
+            print("Задачи с таким номером нет!")
+            return
+        
+        index = num - 1
+        removed = tasks[index]
+        
+        # Удаляем через срезы и сложение (ты теперь это знаешь!)
+        tasks = tasks[:index] + tasks[index+1:]
+        
+        print(f"Задача '{removed}' удалена!")
+        
+    except ValueError:
+        print("Ошибка! Введите число.")
+
+def do_list():
+    if len(tasks) == 0:
+        print("Список пустой!")
+        return
+
+    print("Ваши задачи:")
+    for i in range(len(tasks)):
+        print(f"{i+1}. {tasks[i]}")
+
+def to_do_list():
+
+    print("Список дел открыт!")
+    print("Список дел:\n1. Добавить задачу\n2. Показать все задачи\n3. Удалить задачу\n4. Вернуться в Главное меню\n")
+
+    while True:
+        try:
+
+            command = int(input("Введите номер команды: "))
+
+            if command == 1:
+                do_add()
+            elif command == 2:
+                do_list()
+            elif command == 3:
+                do_del()
+            elif command == 4:
+                break
+
+        except ValueError:
+            print("Нужно ввести число!")
+
 def menu():
     print("Добро пожаловать в помощника!")
 
     while True:
         try:
-            print("=" * 50 + "\nВыбери действие из меню:\n1. Калькулятор\n2. Показать настройки\n3. Выйти из помощника\n" + "=" * 50 + "\n")
+            print("=" * 50 + "\nВыбери действие из меню:\n1. Калькулятор\n2. Показать настройки\n3. Список дел\n4. Выйти из помощника\n" + "=" * 50 + "\n")
 
             command = int(input("\nВведите номер команды из меню: "))
 
@@ -82,8 +149,10 @@ def menu():
             elif command == 2:
                 print("Открываю настройки!\n")
                 print("Настройки пока не реализованы")
-                continue
             elif command == 3:
+                print("Открываю список дел!\n")
+                to_do_list()
+            elif command == 4:
                 print("До свидания!")
                 break
 
@@ -91,7 +160,7 @@ def menu():
             print("Ошибка! Введите число.")
 
 def main():
-    answer()
+#    answer()
     menu()
 
 if __name__ == "__main__":
